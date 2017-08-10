5da2124aecb0ade6fd5e2ff32640f9bf9c3e459d
Matt West <matt@codeslo.com>
2017-08-10 14:08:46 -0700

    Both Percolate and PercolationVisualizer are working

--- algorithm-homework/src/Percolation.java
+++ algorithm-homework/src/Percolation.java
@@ -12,9 +12,6 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
 	// grid is a 2D array with each site either zero(blocked) or one(open)
-	// Percolation application doesn't allow grid row 0 or column 0 as "real" sites.
-	// Row 0 holds two virtual sites at 0,0 and 0,1
-	// grid size is n+1 x n+1 to compensate for unused row 0 and col 0.
 	private int[][] grid;
 
 	// connections tracks union find connected components
@@ -30,18 +27,21 @@ public class Percolation {
 	private int N;
 
 	// size is length of the connections array
-	private int size = N ^ 2 + 2;
+	private int size;
 
 	// constructor
 	public Percolation(int n) {
 		// validate n
-		if (N <= 0) {
+		if (n <= 0) {
 			throw new java.lang.IllegalArgumentException();
 		}
 		// initialize all fields
 		this.N = n;
+		int nSquared = N * N;
+		// grid sites default to zero
+		grid = new int[N][N];
 		// size is (N^2+2) to accomodate virutal sites.
-		this.size = n ^ 2 + 2;
+		this.size = nSquared + 2;
 		// create array + 2 extra id locations for virtual sites. 0 and
 		// connections.length -1 will be those.
 		this.connections = new WeightedQuickUnionUF(size);
@@ -53,8 +53,9 @@ public class Percolation {
 			// connect 0 index to top row
 			connections.union(0, i);
 			// connect last element to bottom row
-			connections.union(size -1, (size -1) -i);
-			// connect the one virtual site in fullSites to the top row for percolation
+			connections.union(size - 1, (nSquared +1) - i);
+			// connect the one virtual site in fullSites to the top row. This is our
+			// 'source' for filling sites. Any open site connected to this site is full.
 			fullSites.union(0, i);
 		}
 
@@ -64,36 +65,49 @@ public class Percolation {
 	public void open(int row, int col) {
 		// validate input
 		validateIndicies(row, col);
-
 		// Mark site as open. If a site in the grid is closed it has a 0 value. If open,
-		// 1.
-		
+		// 1
+		grid[row-1][col-1] = 1;
+		// increment openSites
+		openSites++;
+		// get index conversion for site
+		int index = gridToIndex(row, col);
+
+		// check neighbors for openness. If open, join them. Also join in fullSites
+		// check top neighbor
+		if (row > 1 && isOpen(row - 1, col)) {
+			connections.union(index, gridToIndex(row - 1, col));
+			fullSites.union(index, gridToIndex(row - 1, col));
 		}
-		// check neighbor sites. If they are open, union to them.
-		// find percolationArray position based on row and col
+		// check right neighbor
+		if (col < N && isOpen(row, col + 1)) {
+			connections.union(index, gridToIndex(row, col + 1));
+			fullSites.union(index, gridToIndex(row, col + 1));
+		}
+		// check bottom neighbor
+		if (row < N && isOpen(row + 1, col)) {
+			connections.union(index, gridToIndex(row + 1, col));
+			fullSites.union(index, gridToIndex(row + 1, col));
+		}
+		// check left neighbor
+		if (col > 1 && isOpen(row, col - 1)) {
+			connections.union(index, gridToIndex(row, col - 1));
+			fullSites.union(index, gridToIndex(row, col - 1));
+		}
 
-		int arrayPos = gridToArray(row, col);
-		// top neighbor
-		int top = arrayPos - grid.length;
-		// bottom neighbor
-		int bottom = arrayPos + grid.length;
-		// left neightbor
-		int left = arrayPos - 1;
-		// right neighbor
-		int right = arrayPos + 1;
-
-		int[] neighbors = { top, bottom, left, right };
-
-		
+	}
 
 	// is site open?
 	public boolean isOpen(int row, int col) {
-		return grid[row][col] > 0;
+		validateIndicies(row, col);
+		return grid[row -1][col -1] == 1;
 	}
 
 	// is site full?
 	public boolean isFull(int row, int col) {
-		return grid[row][col] == 2;
+		validateIndicies(row, col);
+		return isOpen(row,col) && fullSites.connected(gridToIndex(row, col), 0);
+
 	}
 
 	// number of open sites
@@ -103,37 +117,38 @@ public class Percolation {
 
 	// does the system percolate?
 	public boolean percolates() {
-		return fullSites[0][1] == 1;
+		if (N == 1) {
+			return isOpen(1, 1);
+		}
+		// this is where we use the virtual sites
+		return connections.connected(0, size - 1);
 	}
 
 	// optional test client
 	public static void main(String[] args) {
+//		Percolation perc = new Percolation(10);
+//		perc.open(1, 1);
+//		perc.open(2, 1);
+//		System.out.println(perc.connections.connected(perc.gridToIndex(1, 1), perc.gridToIndex(1, 4)));
+//
+//		System.out.println(perc.connections.connected(perc.gridToIndex(1, 7), perc.gridToIndex(2, 1)));
+		
 
 	}
 
-	// private methods
-
-	// gridToArray translates row and col into an int that corresponds to a site in
-	// percolationArray
-	private int gridToArray(int row, int col) {
-		return ((row) * 10) + (col);
-	}
-
-	// array to grid translates an array number to an x,y location
-	private int[] arrayToGrid(int site) {
-		int row = site / 10;
-		int col = site % 10;
-		int[] result = { row, col };
-		return result;
+	// gridToArray translates row and col into an index number
+	private int gridToIndex(int row, int col) {
+		validateIndicies(row, col);
+		return ((row - 1) * N + col);
 	}
 
 	// validate idicies. No zeros.
 	private void validateIndicies(int row, int col) {
-		if (row <= 0 || row > n) {
+		if (row <= 0 || row > N) {
 			throw new IndexOutOfBoundsException("row index " + row + " out of bounds");
 		}
 
-		if (col <= 0 || col > n) {
+		if (col <= 0 || col > N) {
 			throw new IndexOutOfBoundsException("column index " + col + " out of bounds");
 		}
 	}
